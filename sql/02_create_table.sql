baea79e5592e65cf3cb66605b36d1c990e8c28e7
sarvesh-surve-2294 <sarveshsurve2222@gmail.com>
2026-07-09 02:57:01 +0530

    02_create_tables.sql

---
+++ sql/02_create_table.sql
@@ -0,0 +1,152 @@
+-- ============================================================
+-- Project : E-Commerce Sales & Customer Analytics
+-- File    : 02_create_tables.sql
+-- Database: ecommerce_sales
+-- ============================================================
+
+USE ecommerce_sales;
+
+-- ============================================================
+-- CUSTOMERS
+-- ============================================================
+
+CREATE TABLE customers (
+    customer_id VARCHAR(32) PRIMARY KEY,
+    customer_unique_id VARCHAR(32) NOT NULL,
+    customer_zip_code_prefix INT,
+    customer_city VARCHAR(100),
+    customer_state CHAR(2)
+);
+
+-- ============================================================
+-- GEOLOCATION
+-- ============================================================
+
+CREATE TABLE geolocation (
+    geolocation_zip_code_prefix INT,
+    geolocation_lat DECIMAL(10,7),
+    geolocation_lng DECIMAL(10,7),
+    geolocation_city VARCHAR(100),
+    geolocation_state CHAR(2)
+);
+
+-- ============================================================
+-- SELLERS
+-- ============================================================
+
+CREATE TABLE sellers (
+    seller_id VARCHAR(32) PRIMARY KEY,
+    seller_zip_code_prefix INT,
+    seller_city VARCHAR(100),
+    seller_state CHAR(2)
+);
+
+-- ============================================================
+-- PRODUCTS
+-- ============================================================
+
+CREATE TABLE products (
+    product_id VARCHAR(32) PRIMARY KEY,
+    product_category_name VARCHAR(100),
+    product_name_lenght INT,
+    product_description_lenght INT,
+    product_photos_qty INT,
+    product_weight_g INT,
+    product_length_cm INT,
+    product_height_cm INT,
+    product_width_cm INT
+);
+
+-- ============================================================
+-- PRODUCT CATEGORY TRANSLATION
+-- ============================================================
+
+CREATE TABLE product_category_translation (
+    product_category_name VARCHAR(100) PRIMARY KEY,
+    product_category_name_english VARCHAR(100)
+);
+
+-- ============================================================
+-- ORDERS
+-- ============================================================
+
+CREATE TABLE orders (
+    order_id VARCHAR(32) PRIMARY KEY,
+    customer_id VARCHAR(32),
+    order_status VARCHAR(20),
+    order_purchase_timestamp DATETIME,
+    order_approved_at DATETIME,
+    order_delivered_carrier_date DATETIME,
+    order_delivered_customer_date DATETIME,
+    order_estimated_delivery_date DATETIME,
+
+    CONSTRAINT fk_orders_customer
+    FOREIGN KEY (customer_id)
+    REFERENCES customers(customer_id)
+);
+
+-- ============================================================
+-- ORDER ITEMS
+-- ============================================================
+
+CREATE TABLE order_items (
+    order_id VARCHAR(32),
+    order_item_id INT,
+    product_id VARCHAR(32),
+    seller_id VARCHAR(32),
+    shipping_limit_date DATETIME,
+    price DECIMAL(10,2),
+    freight_value DECIMAL(10,2),
+
+    PRIMARY KEY (order_id, order_item_id),
+
+    CONSTRAINT fk_orderitems_orders
+    FOREIGN KEY (order_id)
+    REFERENCES orders(order_id),
+
+    CONSTRAINT fk_orderitems_products
+    FOREIGN KEY (product_id)
+    REFERENCES products(product_id),
+
+    CONSTRAINT fk_orderitems_sellers
+    FOREIGN KEY (seller_id)
+    REFERENCES sellers(seller_id)
+);
+
+-- ============================================================
+-- ORDER PAYMENTS
+-- ============================================================
+
+CREATE TABLE order_payments (
+    order_id VARCHAR(32),
+    payment_sequential INT,
+    payment_type VARCHAR(30),
+    payment_installments INT,
+    payment_value DECIMAL(10,2),
+
+    PRIMARY KEY (order_id, payment_sequential),
+
+    CONSTRAINT fk_payments_orders
+    FOREIGN KEY (order_id)
+    REFERENCES orders(order_id)
+);
+
+-- ============================================================
+-- ORDER REVIEWS
+-- ============================================================
+
+CREATE TABLE order_reviews (
+    review_id VARCHAR(32),
+    order_id VARCHAR(32),
+    review_score INT,
+    review_comment_title TEXT,
+    review_comment_message TEXT,
+    review_creation_date DATETIME,
+    review_answer_timestamp DATETIME,
+
+    PRIMARY KEY (review_id),
+
+    CONSTRAINT fk_reviews_orders
+    FOREIGN KEY (order_id)
+    REFERENCES orders(order_id)
+);
