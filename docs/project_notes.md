37ce12a860c893563693200b5e11f37e76a7bb93
sarvesh-surve-2294 <sarveshsurve2222@gmail.com>
2026-07-09 02:54:22 +0530

    Initial project setup and database schema

---
+++ docs/project_notes.md
@@ -0,0 +1,9 @@
+# Project Notes
+
+## Day 1
+
+- Created project structure
+- Downloaded Brazilian E-commerce Dataset
+- Organized files
+- Started data understanding
+
---
+++ sql/01_database_creation.sql
@@ -0,0 +1,14 @@
+-- ============================================
+-- Project : E-Commerce Sales & Customer Analytics
+-- Database Creation Script
+-- Author : Sarvesh Surve
+-- ============================================
+
+DROP DATABASE IF EXISTS ecommerce_sales;
+
+CREATE DATABASE ecommerce_sales;
+
+USE ecommerce_sales;
+
+
+
